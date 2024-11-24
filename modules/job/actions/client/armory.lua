20142da15e70418385814c90d156afbcf9668ecf
Matti <mattiverlinden@gmail.com>
2024-11-24 18:31:33 +0100

    Added multiple equipments support + added nl.json
    Yes, some strings are with "", instead of '', because I use an extension to style my lua code :))
    
    I only changed the armory, nothing else, if something does not work, let me know. Some prop placements could be wrong for the food, I'm not spending my time on "perfecting" that, sorry

--- modules/job/actions/client/armory.lua
+++ modules/job/actions/client/armory.lua
@@ -12,30 +12,25 @@ local ClearPedTasks = ClearPedTasks
 local IsEntityPlayingAnim = IsEntityPlayingAnim
 local TriggerServerEvent = TriggerServerEvent
 
-local function getEquipment(data)
-    utils.debug("Getting equipment")
-
+local function giveEquipment(equipmentBundle, data)
     local playerPed = cache.ped
     local policePed = data.entity
+    local animDict = lib.requestAnimDict("mp_cop_armoury")
 
     SetEntityCoords(playerPed, data.playerPos.x, data.playerPos.y, data.playerPos.z)
     SetEntityHeading(playerPed, data.playerPos.w)
-    SetCurrentPedWeapon(playerPed, joaat('weapon_unarmed'), true)
+    SetCurrentPedWeapon(playerPed, joaat("weapon_unarmed"), true)
 
-    local animDict = lib.requestAnimDict('mp_cop_armoury')
-    local jobGrade = getPlayerJobGrade()
-    local equipment = data.equipment[jobGrade] or data.equipment[1]
+    for _, equipmentItem in ipairs(equipmentBundle) do
+        local playerItem = exports.ox_inventory:Search("count", equipmentItem.item) or 0
 
-    for _, equipmentItem in ipairs(equipment) do
-        local playerItem = exports.ox_inventory:Search('count', equipmentItem.item) or 0
-
-        if type(playerItem) == 'table' then
+        if type(playerItem) == "table" then
             playerItem = playerItem[string.upper(equipmentItem.item)]
         end
 
         if data.require_storage then
-            local storageItem = lib.callback.await('ars_policejob:getItemCount', false, data.storage.stashId, equipmentItem.item)
-            if type(storageItem) == 'table' then
+            local storageItem = lib.callback.await("ars_policejob:getItemCount", false, data.storage.stashId, equipmentItem.item)
+            if type(storageItem) == "table" then
                 storageItem = storageItem[string.upper(equipmentItem.item)]
             end
 
@@ -44,8 +39,7 @@ local function getEquipment(data)
                     local itemToGive = storageItem > equipmentItem.quantity and equipmentItem.quantity or storageItem
                     utils.debug("Getting item" .. equipmentItem.item .. " quantity: " .. itemToGive - playerItem)
 
-                    TaskPlayAnim(policePed, animDict, 'pistol_on_counter_cop', 1.0, -1, 1.0, 0, 0, 0, 0, 0)
-
+                    TaskPlayAnim(policePed, animDict, "pistol_on_counter_cop", 1.0, -1, 1.0, 0, 0, 0, 0, 0)
                     Wait(1100)
 
                     local itemModel = lib.requestModel(equipmentItem.prop.model)
@@ -53,7 +47,7 @@ local function getEquipment(data)
                     local object = CreateObject(itemModel, playerCoords.x, playerCoords.y, playerCoords.z + 1, true, true, true)
 
                     AttachEntityToEntity(object, policePed, GetPedBoneIndex(policePed, 57005), 0, 0, -0, 0, 0, 0, true, true, false, true, 1, true)
-                    TaskPlayAnim(playerPed, animDict, 'pistol_on_counter', 1.0, -1, 1.0, 0, 0, 0, 0, 0)
+                    TaskPlayAnim(playerPed, animDict, "pistol_on_counter", 1.0, -1, 1.0, 0, 0, 0, 0, 0)
 
                     Wait(2000)
 
@@ -64,34 +58,32 @@ local function getEquipment(data)
                     Wait(2000)
                     AttachEntityToEntity(placedObject, playerPed, GetPedBoneIndex(playerPed, 57005), 0, 0, -0, -0, 0, -0, true, true, false, true, 1, true)
 
-                    while IsEntityPlayingAnim(playerPed, animDict, 'pistol_on_counter', 3) do
+                    while IsEntityPlayingAnim(playerPed, animDict, "pistol_on_counter", 3) do
                         Wait(0)
                     end
 
                     ClearPedTasks(policePed)
-                    TriggerServerEvent('ars_policejob:giveItemToPlayer', data.storage.stashId, equipmentItem.item, itemToGive - playerItem, true, data.jobs)
+                    TriggerServerEvent("ars_policejob:giveItemToPlayer", data.storage.stashId, equipmentItem.item, equipmentItem.quantity - playerItem, true, data.jobs)
                     DeleteEntity(placedObject)
                 else
-                    utils.showNotification(equipmentItem.label .. ' ' .. locale('already_have_item'))
+                    utils.showNotification(locale("already_have_item", equipmentItem.label))
                 end
             else
-                utils.showNotification(equipmentItem.label .. ' ' .. locale('no_item_in_storage'))
+                utils.showNotification(equipmentItem.label .. " " .. locale("no_item_in_storage"))
             end
         else
             if playerItem < equipmentItem.quantity then
                 utils.debug("Getting item" .. equipmentItem.item .. " quantity: " .. equipmentItem.quantity - playerItem)
 
-                TaskPlayAnim(policePed, animDict, 'pistol_on_counter_cop', 1.0, -1, 1.0, 0, 0, 0, 0, 0)
-
+                TaskPlayAnim(policePed, animDict, "pistol_on_counter_cop", 1.0, -1, 1.0, 0, 0, 0, 0, 0)
                 Wait(1100)
 
                 local itemModel = lib.requestModel(equipmentItem.prop.model)
                 local playerCoords = cache.coords
-                local object = CreateObject(itemModel, playerCoords.x, playerCoords.y, playerCoords.z + 1, true, true,
-                    true)
+                local object = CreateObject(itemModel, playerCoords.x, playerCoords.y, playerCoords.z + 1, true, true, true)
 
                 AttachEntityToEntity(object, policePed, GetPedBoneIndex(policePed, 57005), 0, 0, -0, 0, 0, 0, true, true, false, true, 1, true)
-                TaskPlayAnim(playerPed, animDict, 'pistol_on_counter', 1.0, -1, 1.0, 0, 0, 0, 0, 0)
+                TaskPlayAnim(playerPed, animDict, "pistol_on_counter", 1.0, -1, 1.0, 0, 0, 0, 0, 0)
 
                 Wait(2000)
 
@@ -102,17 +94,54 @@ local function getEquipment(data)
                 Wait(2000)
                 AttachEntityToEntity(placedObject, playerPed, GetPedBoneIndex(playerPed, 57005), 0, 0, -0, -0, 0, -0, true, true, false, true, 1, true)
 
-                while IsEntityPlayingAnim(playerPed, animDict, 'pistol_on_counter', 3) do
+                while IsEntityPlayingAnim(playerPed, animDict, "pistol_on_counter", 3) do
                     Wait(0)
                 end
 
                 ClearPedTasks(policePed)
-                TriggerServerEvent('ars_policejob:giveItemToPlayer', data.storage.stashId, equipmentItem.item, equipmentItem.quantity - playerItem, false, data.jobs)
+                TriggerServerEvent("ars_policejob:giveItemToPlayer", nil, equipmentItem.item, equipmentItem.quantity - playerItem, true, data.jobs)
                 DeleteEntity(placedObject)
             else
-                utils.showNotification(equipmentItem.label .. ' ' .. locale('already_have_item'))
+                utils.showNotification(equipmentItem.label .. " " .. locale("already_have_item"))
             end
         end
+    end
+end
+
+local function getEquipment(data)
+    utils.debug("Getting equipment")
+    local jobGrade = getPlayerJobGrade()
+    local equipmentOptions = {}
+
+    for grade = 0, #data.equipment do
+        if grade <= jobGrade then
+            local equipmentBundle = data.equipment[grade]
+            local equipmentName = equipmentBundle.name
+            local equipmentIcon = equipmentBundle.icon
+            local equipmentItems = {}
+            for _, item in ipairs(equipmentBundle) do
+                table.insert(equipmentItems, item.label .. " (" .. tostring(item.quantity) .. "x)")
+            end
+            table.insert(equipmentOptions, {
+                title = equipmentName,
+                description = table.concat(equipmentItems, ", "),
+                icon = equipmentIcon,
+                onSelect = function()
+                    giveEquipment(equipmentBundle, data)
+                end,
+            })
+        end
+    end
+
+    if #equipmentOptions > 0 then
+        lib.registerContext({
+            id = "select_equipment_bundle",
+            title = locale("armory_interact_label"),
+            options = equipmentOptions,
+        })
+        lib.showContext("select_equipment_bundle")
+    else
+        utils.showNotification(locale("no_equipment"))
     end
 end
 
@@ -120,12 +149,12 @@ function initArmory(data)
     local ped = utils.createPed(data.model, data.pedPos)
     local options = {
         {
-            name = 'armory' .. ped,
-            label = locale('armory_interact_label'),
-            icon = 'fa-solid fa-road',
+            name = "armory" .. ped,
+            label = locale("armory_interact_label"),
+            icon = "fa-solid fa-boxes-stacked",
             groups = data.jobs,
             canInteract = function(entity, distance, coords, name, bone)
-                if IsEntityPlayingAnim(entity, 'mp_cop_armoury', 'pistol_on_counter_cop', 3) or IsEntityPlayingAnim(entity, 'mp_cop_armoury', 'rifle_on_counter_cop', 3) then
+                if IsEntityPlayingAnim(entity, "mp_cop_armoury", "pistol_on_counter_cop", 3) or IsEntityPlayingAnim(entity, "mp_cop_armoury", "rifle_on_counter_cop", 3) then
                     return false
                 end
 
@@ -134,19 +163,19 @@ function initArmory(data)
             onSelect = function(entity)
                 data.entity = entity.entity
                 getEquipment(data)
-            end
+            end,
         },
         {
-            name = 'armory_storage' .. ped,
-            label = locale('armory_interact_storage_label'),
-            icon = 'fa-solid fa-road',
+            name = "armory_storage" .. ped,
+            label = locale("armory_interact_storage_label"),
+            icon = "fa-solid fa-boxes-stacked",
             canInteract = function(entity, distance, coords, name, bone)
                 return data.require_storage and hasJob(data.jobs) and getPlayerJobGrade() >= data.storage.minGradeAccess and player.inDuty()
             end,
             onSelect = function(entity)
-                exports.ox_inventory:openInventory('stash', data.storage.stashId)
-            end
-        }
+                exports.ox_inventory:openInventory("stash", data.storage.stashId)
+            end,
+        },
     }
     exports.ox_target:addLocalEntity(ped, options)
 end
